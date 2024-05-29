0ddc55664980752e506d77479d25ec15ed65fba2
aiti.k <sunny20747@gmail.com>
2024-05-29 16:59:55 +0700

    up

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,19 @@
+using System;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    // [Route("api/[controller]")]
+    // [ApiController]
+
+    // public class UsersController : ControllerBase 
+    // {
+
+    //     [HttpGet("hello")]
+    //     public IActionResult Hello()
+    //     {
+    //         return Ok("Hello");
+    //     }
+    // }
+}
---
+++ Controllers/indexController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+namespace api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IndexController : ControllerBase
+    {
+        
+        [HttpGet("hello")]
+        public IActionResult Hello()
+        {
+            Console.WriteLine("Hello");
+            return Ok("Hello");
+        }
+
+    }
+
+    [Route("api/[controller]")]
+    public class Index1Controller : ControllerBase
+    {
+        [HttpGet("hello1")]
+        public IActionResult Hello1()
+        {
+            return Ok("Hello");
+        }
+
+    }
+}
